3dd4396e6f16239a60c90ea3b1a1bbedb2c3de74
Jourdin Pierre <pierre.jourdin@student-cs.fr>
2025-01-21 17:27:35 +0100

    maybe solve2 works

--- solve2.py
+++ solve2.py
@@ -3,7 +3,8 @@ import csv
 import json
 import matplotlib.pyplot as plt
 
-epsilon = 0.001
+epsilon = 0.03
+bigInt = 1000000
 LOWER_WORKLOAD = 0.8
 UPPER_WORKLOAD = 1.2
 
@@ -36,7 +37,8 @@ N_SR = len(initial_repartition_idx)
 N_bricks = len(brick_workload)
 
 
-def compute_distances(vars: list[list[Var]]) -> LinExpr:
+def compute_distances(fullvars: list[list[list[Var]]]) -> LinExpr:
+    vars = fullvars[1]
     halfDistances = LinExpr()
     for sr_idx in range(N_SR):
         for brick in range(N_bricks):
@@ -44,7 +46,8 @@ def compute_distances(vars: list[list[Var]]) -> LinExpr:
     return 2 * halfDistances  # parce que aller-retour
 
 
-def compute_workloads(vars: list[list[Var]]) -> list[LinExpr]:
+def compute_workloads(fullvars: list[list[list[Var]]]) -> list[LinExpr]:
+    vars = fullvars[0]
     workloads = [LinExpr() for _ in range(N_SR)]
     for sr_idx in range(N_SR):
         for brick in range(N_bricks):
@@ -52,7 +55,8 @@ def compute_workloads(vars: list[list[Var]]) -> list[LinExpr]:
     return workloads
 
 
-def compute_disruption(vars: list[list[Var]]) -> LinExpr:
+def compute_disruption(fullvars: list[list[list[Var]]]) -> LinExpr:
+    vars = fullvars[0]
     halfDisruption = LinExpr()
     for brick in range(N_bricks):
         sr_idx = initial_repartition[brick]
@@ -83,7 +87,19 @@ def compute_solutions(m, vars):
 def main():
     # initialize model
     m = Model("solve")
-    vars: list[list[Var]] = []
+    vars: list[list[list[Var]]] = []
+
+    assignations: list[list[Var]] = []  # floats between 0 and 1
+
+    haveToTravel: list[
+        list[Var]
+    ] = []  # ints : 0 or 1. must be 1 iff the corresponding var in assignations is not 0
+
+    b: list[
+        list[Var]
+    ] = []  # helping to ensure the correspondence between assignations and haveToTravel
+
+    vars = [assignations, haveToTravel, b]
 
     ###############
     # In our model, the ith variable represent which SR the ith block is allocated to
@@ -93,8 +109,27 @@ def main():
     for brick in range(N_bricks):
         v = []
         for sr_idx in range(N_SR):
-            v.append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, ub=1))
-        vars.append(v)
+            v.append(
+                m.addVar(
+                    vtype=GRB.CONTINUOUS,
+                    lb=0,
+                    ub=1,
+                    name=f"assignations{brick},{sr_idx}",
+                )
+            )
+        vars[0].append(v)
+
+    for brick in range(N_bricks):
+        v = []
+        for sr_idx in range(N_SR):
+            v.append(m.addVar(vtype=GRB.BINARY, name=f"haveToTravel{brick},{sr_idx}"))
+        vars[1].append(v)
+
+    for brick in range(N_bricks):
+        v = []
+        for sr_idx in range(N_SR):
+            v.append(m.addVar(vtype=GRB.BINARY, name=f"b{brick},{sr_idx}"))
+        vars[2].append(v)
 
     # create constraints
 
@@ -104,41 +139,53 @@ def main():
         m.addConstr(LOWER_WORKLOAD <= workloads[sr_idx])
         m.addConstr(workloads[sr_idx] <= UPPER_WORKLOAD)
 
-    # 1 SR per brick
+    # 1 SR equivalent per brick
     for brick in range(N_bricks):
         sum = LinExpr()
         for sr_idx in range(N_SR):
-            sum += vars[brick][sr_idx]
+            sum += vars[0][brick][sr_idx]
         m.addConstr(sum == 1)
 
+    # Matching between assignations and haveToTravel
+    for brick in range(N_bricks):
+        for sr_idx in range(N_SR):
+            m.addConstr(
+                vars[0][brick][sr_idx] + vars[1][brick][sr_idx]
+                <= 0 + bigInt * vars[2][brick][sr_idx]
+            )
+            m.addConstr(
+                vars[0][brick][sr_idx] + vars[1][brick][sr_idx]
+                >= 1 + epsilon - bigInt * (1 - vars[2][brick][sr_idx])
+            )
+
     # Disruption
-    m.setObjective(compute_disruption(vars), GRB.MINIMIZE)
-    m.optimize()
-    print("Disruption: ", m.objVal)
+    # m.setObjective(compute_disruption(vars), GRB.MINIMIZE)
+    # m.optimize()
+    # print("Disruption: ", m.objVal)
     # Best disruption : 0.3391
 
     # Distance
-    m.setObjective(compute_distances(vars), GRB.MINIMIZE)
-    m.optimize()
-    print("Distance: ", m.objVal)
+    # m.setObjective(compute_distances(vars), GRB.MINIMIZE)
+    # m.optimize()
+    # print("Distance: ", m.objVal)
     # Best distance : 309.24
 
     # Multi-objective, with epsilon-constraint strategy
     # We fix the disruption, and optimize the distance
-    # best_solutions = compute_solutions(m, vars)
+    best_solutions = compute_solutions(m, vars)
 
-    # print("number of solutions:", len(best_solutions))
+    print("number of solutions:", len(best_solutions))
 
-    # plt.figure(figsize=(10, 6))
-    # plt.scatter(
-    #     [solution[0] for solution in best_solutions],
-    #     [solution[1] for solution in best_solutions],
-    # )
-    # plt.xlabel("Distance")
-    # plt.ylabel("Disruption")
-    # plt.title("Distance vs Disruption")
-    # plt.grid(True)
-    # plt.show()
+    plt.figure(figsize=(10, 6))
+    plt.scatter(
+        [solution[0] for solution in best_solutions],
+        [solution[1] for solution in best_solutions],
+    )
+    plt.xlabel("Distance")
+    plt.ylabel("Disruption")
+    plt.title("Distance vs Disruption")
+    plt.grid(True)
+    plt.show()
 
 
 if __name__ == "__main__":
